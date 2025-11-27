42804922dbffed1a5f7593beba59ce3c9bb480a5
dkphhh <dkphhh@foxmail.com>
2025-11-27 19:13:35 +0800

    update

--- src/pages/formEdit/App.svelte
+++ src/pages/formEdit/App.svelte
@@ -1,30 +1,53 @@
 <script lang="ts">
 	import Layout from '@/components/layout/Layout.svelte';
-	import {
-		SheetForm,
-		BitableForm,
-		DocForm,
-		getFormById
-	} from '@/components/settings/settings.svelte';
 	import SheetFormSetting from '@/components/forms/SheetFormSetting.svelte';
 	import BitableFormSetting from '@/components/forms/BitableFormSetting.svelte';
 	import DocFormSetting from '@/components/forms/DocFormSetting.svelte';
+
+	import { getForm } from '@/components/forms/forms.svelte';
 
 	const searchParams = new URL(window.location.toString()).searchParams;
 	const mode = searchParams.get('mode') as EditMode;
 	const formType = searchParams.get('type') as FormTypeName;
 	const formId = searchParams.get('formId');
-	const form = formId ? getFormById(formId) : null;
+	const form = formId ? getForm(formId) : null;
 </script>
 
 <Layout>
 	{#if mode == 'create'}
 		{#if formType == 'sheet'}
-			<SheetFormSetting form={new SheetForm()} />
+			<SheetFormSetting
+				form={{
+					formType: '飞书表格',
+					id: '',
+					name: '',
+					rangeIndex: {
+						endIndex: '',
+						startIndex: ''
+					},
+					sheetId: '',
+					sheetToken: ''
+				}}
+			/>
 		{:else if formType == 'bitable'}
-			<BitableFormSetting form={new BitableForm()} />
+			<BitableFormSetting
+				form={{
+					formType: '多维表格',
+					id: '',
+					name: '',
+					appToken: '',
+					tableId: ''
+				}}
+			/>
 		{:else if formType == 'doc'}
-			<DocFormSetting form={new DocForm()} />
+			<DocFormSetting
+				form={{
+					formType: '飞书文档',
+					id: '',
+					name: '',
+					folderToken: ''
+				}}
+			/>
 		{/if}
 	{:else if mode == 'edit' && form}
 		{#if form.formType == '飞书表格'}
